f6030afbf877723d6175c3232af0337a5aed28b8
JiwanKim <jisungyeah@gmail.com>
2026-06-12 13:43:36 +0900

    오타 수정

--- backend/src/main/java/io/streak/habitflow/domain/task/dto/response/TaskListQuery.java
+++ backend/src/main/java/io/streak/habitflow/domain/task/dto/response/TaskListQuery.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public class TaskListQuery {
-    private Long Id;
+    private Long id;
     private String name;
     private String description;
     private TaskPriorityType taskPriorityType;
