fb8b6780bd78fc710aee43fcf492ff32f6c7989e
JiwanKim <jisungyeah@gmail.com>
2026-06-17 18:31:22 +0900

    사용하지 않는 변수 삭제

--- backend/src/main/java/io/streak/habitflow/domain/activitylog/dto/request/ActivityLogRequest.java
+++ backend/src/main/java/io/streak/habitflow/domain/activitylog/dto/request/ActivityLogRequest.java
@@ -3,10 +3,6 @@ package io.streak.habitflow.domain.activitylog.dto.request;
 import io.streak.habitflow.domain.task.type.ActivityType;
 import io.streak.habitflow.domain.task.type.TargetType;
 import lombok.Builder;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public final class ActivityLogRequest{
     @Builder
@@ -15,10 +11,4 @@ public final class ActivityLogRequest{
             TargetType targetType,
             ActivityType activityType,
             String customMessage){}
-    public record SearchCondition(
-            List<Long> projectIds,
-            List<Long> memberIds,
-            @DateTimeFormat(pattern = "yyyy-MM-dd")
-            LocalDate targetDate
-    ){}
 }
--- backend/src/main/java/io/streak/habitflow/domain/activitylog/repository/ActivityLogRepository.java
+++ backend/src/main/java/io/streak/habitflow/domain/activitylog/repository/ActivityLogRepository.java
@@ -3,8 +3,5 @@ package io.streak.habitflow.domain.activitylog.repository;
 import io.streak.habitflow.domain.activitylog.entity.ActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ActivityLogRepository extends JpaRepository<ActivityLog,Long>, ActivityLogRepositoryCustom {
-    List<ActivityLog> findByMemberId(Long memberId);
 }
