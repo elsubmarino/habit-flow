248e1076cd5170057e9992e6794b49816ecb7f7e
JiwanKim <jisungyeah@gmail.com>
2026-06-18 22:38:08 +0900

    Member 테이블에 email 컬럼 unique 추가

--- backend/src/main/java/io/streak/habitflow/domain/member/entity/Member.java
+++ backend/src/main/java/io/streak/habitflow/domain/member/entity/Member.java
@@ -19,6 +19,8 @@ public class Member extends BaseTimeEntity {
 
     private String name;
     private String password;
+
+    @Column(unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
