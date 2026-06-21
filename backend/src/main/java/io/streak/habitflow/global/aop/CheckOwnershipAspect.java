5d52de01c4813f1dea47b1661e6012cc46e66b5c
JiwanKim <jisungyeah@gmail.com>
2026-06-21 19:55:18 +0900

    라벨도 소유자 권한 체크 추가

--- backend/src/main/java/io/streak/habitflow/global/aop/CheckOwnershipAspect.java
+++ backend/src/main/java/io/streak/habitflow/global/aop/CheckOwnershipAspect.java
@@ -2,6 +2,8 @@ package io.streak.habitflow.global.aop;
 
 import io.streak.habitflow.domain.comment.entity.Comment;
 import io.streak.habitflow.domain.comment.repository.CommentRepository;
+import io.streak.habitflow.domain.label.entity.Label;
+import io.streak.habitflow.domain.label.repository.LabelRepository;
 import io.streak.habitflow.domain.member.entity.Member;
 import io.streak.habitflow.domain.member.repository.MemberRepository;
 import io.streak.habitflow.domain.notification.entity.Notification;
@@ -33,6 +35,7 @@ public class CheckOwnershipAspect {
     private final NotificationRepository notificationRepository;
     private final ProjectRepository projectRepository;
     private final ProjectMemberRepository projectMemberRepository;
+    private final LabelRepository labelRepository;
     private final HashidsProvider hashidsProvider;
 
     @Before("@annotation(CheckOwnership) || @annotation(CheckOwnerships)")
@@ -46,40 +49,42 @@ public class CheckOwnershipAspect {
         Object[]args=joinPoint.getArgs();
 
         Long taskId = null;
-        Long memberId = null;
         Long commentId = null;
         Long loginMemberId = null;
         Long notificationId = null;
         Long projectId = null;
+        Long labelId = null;
         TaskRequest.Create request = null;
 
         for(int i=0;i<parameterNames.length;i++){
             if("taskId".equals(parameterNames[i])) taskId = (Long)args[i];
             else if("commentId".equals(parameterNames[i])) commentId = (Long)args[i];
-            else if("memberId".equals(parameterNames[i])) memberId = (Long)args[i];
             else if("loginMemberId".equals(parameterNames[i])) loginMemberId = (Long)args[i];
             else if("notificationId".equals(parameterNames[i])) notificationId = (Long)args[i];
             else if("projectId".equals(parameterNames[i])) projectId = (Long)args[i];
+            else if("labelId".equals(parameterNames[i])) labelId = (Long)args[i];
             else if(args[i] instanceof TaskRequest.Create)request = (TaskRequest.Create)args[i];
 
         }
 
         for(CheckOwnership checkOwnership: checkOwnerships){
             String domainType = checkOwnership.type();
-            if("TASK".equals(domainType) && taskId != null && memberId != null){
-                checkTaskOwner(taskId,memberId);
+            if("TASK".equals(domainType) && taskId != null && loginMemberId != null){
+                checkTaskOwner(taskId,loginMemberId);
             }else if("SUB_TASK".equals(domainType)){
-                if(request != null && request.parentId() != null && memberId != null){
-                    checkTaskOwner(hashidsProvider.decode(request.parentId()),memberId);
+                if(request != null && request.parentId() != null && loginMemberId != null){
+                    checkTaskOwner(hashidsProvider.decode(request.parentId()),loginMemberId);
                 }
-            }else if("COMMENT".equals(domainType) && commentId != null && memberId != null){
-                checkCommentOwner(commentId,memberId);
-            }else if("MEMBER".equals(domainType) && loginMemberId != null && memberId != null){
-                checkMemberOwner(loginMemberId,memberId);
-            }else if("NOTIFICATION".equals(domainType) && notificationId != null && memberId != null){
-                checkNotificationOwner(notificationId,memberId);
-            }else if("PROJECT".equals(domainType) && projectId != null && memberId != null){
-                checkProjectOwner(projectId,memberId);
+            }else if("COMMENT".equals(domainType) && commentId != null && loginMemberId != null){
+                checkCommentOwner(commentId,loginMemberId);
+            }else if("MEMBER".equals(domainType) && loginMemberId != null && loginMemberId != null){
+                checkMemberOwner(loginMemberId,loginMemberId);
+            }else if("NOTIFICATION".equals(domainType) && notificationId != null && loginMemberId != null){
+                checkNotificationOwner(notificationId,loginMemberId);
+            }else if("PROJECT".equals(domainType) && projectId != null && loginMemberId != null) {
+                checkProjectOwner(projectId, loginMemberId);
+            }else if("LABEL".equals(domainType) && labelId != null && loginMemberId != null){
+
             }
         }
     }
@@ -97,6 +102,13 @@ public class CheckOwnershipAspect {
     public void checkCommentOwner(Long commentId, Long memberId){
         Comment comment = commentRepository.getOrThrow(commentId);
         if(!comment.getMember().getId().equals(memberId)){
+            throw new AccessDeniedException("해당 자원에 대한 권한이 없습니다.");
+        }
+    }
+
+    public void checkLabelOwner(Long labelId, Long memberId){
+        Label label = labelRepository.getOrThrow(labelId);
+        if(!label.getMember().getId().equals(memberId)){
             throw new AccessDeniedException("해당 자원에 대한 권한이 없습니다.");
         }
     }
